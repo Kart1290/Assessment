9d8b8e6a8b5adf159a788e8f13869769209abd09
kart1290 <kart1290@gmail.com>
2022-12-31 13:09:43 +0800

    initial commit

---
+++ Nsearch/src/main/java/com/karthic/nsearch/NsearchApplication.java
@@ -0,0 +1,13 @@
+package com.karthic.nsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsearchApplication.class, args);
+	}
+
+}
---
+++ Nsearch/src/test/java/com/karthic/nsearch/NsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthic.nsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
